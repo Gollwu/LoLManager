074d3c697940fbc1dc2d1bd18427c7e6b1baaaea
Nicolas <nikkidbz@gmail.com>
2015-03-29 18:20:12 +0200

    don't care of exemple

--- server/src/controller/player.controller.js
+++ server/src/controller/player.controller.js
@@ -32,18 +32,6 @@ app.param('id', function(req, res, next, id) {
  * @api {get} /api/player Get all the players
  * @apiName GetPlayers
  * @apiGroup Player
- *
- * @apiSuccessExample Success-Response:
- *      HTTP/1.1 200 OK
- *      [
- *          {
- *              "id": 1
- *              "firstname": "John",
- *              "lastname": "Doe",
- *              ...
- *          },
- *              ...
- *      ]
  */
 app.get('/', function(req, res) {
     Player.findAll().then(function(players) {
@@ -59,35 +47,6 @@ app.get('/', function(req, res) {
  * @apiParam {integer} id Id of the player
  * @apiError {404} NotFound The id of the player was not found.
  *
- * @apiSuccessExample Success-Response:
- *      {
- *         "id": 1,
- *         "firstName": "Jeff",
- *         "lastName": "Aubry",
- *         "pseudo": "Golwu",
- *         "birthdate": null,
- *         "country": null,
- *         "role": null,
- *         "warding": 0,
- *         "community": 0,
- *         "attitude": 0,
- *         "charisma": 0,
- *         "adaptability": 0,
- *         "laning": 0,
- *         "farming": 0,
- *         "outplay": 0,
- *         "ganking": 0,
- *         "pressuring": 0,
- *         "smiting": 0,
- *         "ADCarry": 0,
- *         "Tank": 0,
- *         "ADTank": 0,
- *         "APCarry": 0,
- *         "AssassinAD": 0,
- *         "Support": 0,
- *         "createdAt": "2015-03-29T00:31:41.792Z",
- *         "updatedAt": "2015-03-29T00:31:41.792Z"
- *      }
  */
 app.get('/:id', function(req, res) {
     res.send(req.player);
